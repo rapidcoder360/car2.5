b6999cefb65ef494eafb8026c7030574e5d15b37
aditya <deepakojha@outlook.com>
2020-03-03 16:34:25 +0530

    reset the database and mark the player

--- js/Game.js
+++ js/Game.js
@@ -70,7 +70,10 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
-          cars[index - 1].shapeColor = "red";
+          strokeWeight(10);
+          stroke(0);
+          fill("red");
+          ellipse(x,y,70,70);
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
         }
